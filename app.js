acb06d0621a9ff99e40221688ba3c0d6ac5f4492
Amruth Nare <45131882+amruth21@users.noreply.github.com>
2019-06-17 11:44:11 -0400

    initial

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const app = express();
+
+const port = 1234;
+
+app.use(bodyParser.json());
+
+app.use(express.static('src'));
+
+app.get('/', function(req, res) {
+    //res.send("This is home page")
+    res.sendFile(path.join(__dirname, '/src/pages/index.html'))
+})
+
+app.listen(port, function () {
+    console.log('Server running on http://localhost:' + port);
+})
